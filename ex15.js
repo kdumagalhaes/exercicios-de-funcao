e29b196404fa1de4cffc150365b60480351e173e
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-09-19 18:33:31 -0300

    exercise 15 check1

---
+++ ex15.js
@@ -0,0 +1,20 @@
+const choosingCar = (car) => {
+  switch (car) {
+    case 'hatch':
+      return 'Compra efetuada com sucesso.';
+
+    case 'sedan':
+    case 'motocicleta':
+    case 'caminhonete':
+      return 'Tem certeza que não prefere este modelo?';
+
+    default:
+      return 'Não trabalhamos com este tipo de automóvel aqui.';
+  }
+};
+
+console.log(choosingCar('hatch'));
+console.log(choosingCar('sedan'));
+console.log(choosingCar('motocicleta'));
+console.log(choosingCar('caminhonete'));
+console.log(choosingCar('fusquinha'));
