00fe6699fc363d3dc3fdb918e46722a84647e564
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-09-19 19:17:56 -0300

    exercise 18 check

---
+++ ex18.js
@@ -0,0 +1,41 @@
+function readNumber(number) {
+  switch (number) {
+    case 0:
+      console.log('zero');
+      break;
+    case 1:
+      console.log('one');
+      break;
+    case 2:
+      console.log('two');
+      break;
+    case 3:
+      console.log('three');
+      break;
+    case 4:
+      console.log('four');
+      break;
+    case 5:
+      console.log('five');
+      break;
+    case 6:
+      console.log('six');
+      break;
+    case 7:
+      console.log('seven');
+      break;
+    case 8:
+      console.log('eight');
+      break;
+    case 9:
+      console.log('nine');
+      break;
+    case 10:
+      console.log('ten');
+      break;
+    default:
+      console.log('Número fora do intervalo.');
+  }
+};
+
+readNumber(8)
